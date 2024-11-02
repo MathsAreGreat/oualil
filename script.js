3467e19db605b64611e75312d5693bd5ae86fa83
MathsAreGreat <mathsphile@gmail.com>
2024-11-02 15:51:35 +0100

    [20241102-155135] Update puissance 3

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ window.MathJax = {
 (function () {
 	var script = document.createElement("script");
 	script.src = "http://localhost/mathjax/es5/tex-chtml-full.js";
+	script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
 	script.async = true;
 	document.head.appendChild(script);
 })();
